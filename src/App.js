ddd16df1a108730b2826602c5820eea1d9ea071b
pravinsyadav <pravinsy2003@gmail.com>
2024-10-22 00:30:38 +0530

    added new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./Components/Navbar";
 import "./App.css";
-import Textform from "./Components/Textform";
+// import Textform from "./Components/Textform";
+import About from "./Components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="container">
-        <Textform heading="enter the text to analyze" />
+        {/* <Textform heading="enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
